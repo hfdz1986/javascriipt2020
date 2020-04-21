43a764ad930176630452dc0736defe4d7a7ec345
leo <49946423+hfdz1986@users.noreply.github.com>
2020-04-21 15:07:15 +0200

    随机抽奖代码

---
+++ js1.js
@@ -0,0 +1,26 @@
+// 随机抽奖
+
+var lis=document.getElementsByTagName("li");
+var index=0;
+
+var box=document.getElementById("box");
+var time=setInterval(function () {
+
+for(var i=0;i<lis.length;i++){
+
+    lis[i].style.display="none";
+
+}
+    index++;
+if(index>lis.length-1) {
+    index = 0;
+}
+    lis[index].style.display = "block";
+
+
+},1000);
+
+box.onclick=function () {
+    clearInterval(time)
+
+}
